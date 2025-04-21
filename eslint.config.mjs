4b571d5c1961787a8d1f909c0c2c983e2397d9b0
Yuriy Andreev <andreyev.yury@gmail.com>
2025-04-21 22:17:47 +0300

    fix: fixes eslint config file

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,17 +2,12 @@ import js from "@eslint/js";
 import globals from "globals";
 import tseslint from "typescript-eslint";
 import {defineConfig, globalIgnores} from "eslint/config";
-import eslintPluginPrettier from 'eslint-plugin-prettier';
 
 
 export default defineConfig([
   { files: ["**/*.{js,mjs,cjs,ts}"], plugins: { js }, extends: ["js/recommended"] },
   { files: ["**/*.js"], languageOptions: { sourceType: "script" } },
   { files: ["**/*.{js,mjs,cjs,ts}"], languageOptions: { globals: globals.browser } },
-    eslintPluginPrettier,
-  {extends: [
-      "prettier"
-    ]},
   tseslint.configs.recommended,
   globalIgnores(["**/dist", "**/node_modules/", "**/*.config.*"])
 ]);
